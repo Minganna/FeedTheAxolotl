39bfceb0b2cbc043b948bda24697967e82beb41f
Marco Minganna <s4929892@bournemouth.ac.uk>
2018-03-16 12:07:53 +0000

    Started the machine game

---
+++ Assets/Script/Machine/CamerasChanging.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerasChanging : MonoBehaviour {
+	public Camera[] cams;
+	public int switchcameras=1;
+
+
+		
+
+	public void MainCam ()
+	{
+		if (switchcameras == 3) {
+			cams [0].enabled = false;
+			cams [1].enabled = true;
+			cams [2].enabled = false;
+			switchcameras = 0;
+		}
+		
+		if (switchcameras == 2) {
+			cams [0].enabled = false;
+			cams [1].enabled = false;
+			cams [2].enabled = true;
+
+		}
+		if (switchcameras == 1) {
+			cams [0].enabled = true;
+			cams [1].enabled = false;
+			cams [2].enabled = false;
+
+		}
+		switchcameras = switchcameras + 1;
+
+	}
+}
+
---
+++ Assets/Script/Machine/Cubedrag.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cubedrag : MonoBehaviour {
+
+	public Rigidbody rb;
+	public Rigidbody hook;
+	public float MaxDragDistance=2f;
+
+	public float releaseTime=0.15f;
+	private bool isPressed=false;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		if (isPressed) {
+			if (Vector3.Distance (mousePos, hook.position) > MaxDragDistance) {
+				rb.position = hook.position+( mousePos-hook.position).normalized*MaxDragDistance;
+			}
+			else
+				rb.position =mousePos;
+
+		}
+
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+	}
+
+	IEnumerator WaitNextLevel()
+	{
+		yield return new WaitForSeconds (4f);
+	}
+		
+
+	void OnMouseDown()
+	{
+		isPressed = true;
+		rb.isKinematic = true;
+	}
+
+	void OnMouseUp()
+	{
+		isPressed = false;
+		rb.isKinematic = false;
+		StartCoroutine (Release ());
+	}
+
+
+
+	IEnumerator Release ()
+	{
+		yield return new WaitForSeconds (releaseTime);
+
+		GetComponent<SpringJoint2D>().enabled = false;
+		this.enabled = false;
+
+		yield return new WaitForSeconds (0.2f);
+
+
+	}
+
+
+}
